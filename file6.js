307d6abe2c79b517fec6bb58c571cf2b34f3a283
vijaykrishna123 <nimmagadda.vijju@gmail.com>
2017-12-07 22:41:16 -0600

    file6

--- file6.js
+++ file6.js
@@ -1,7 +1,16 @@
 var a="vijay"; 
 function vijay()
 {
-    var a="Krishna";
+    var a="Krishna";//global scope
      return a;
     } 
 vijay();
+var a="rama"; function rama(){var a="Krishna"; return a;} console.log(a);
+
+var file1="new";
+
+funtion new(){
+    Var file1="new2";
+    console.log(file1);
+}
+function foo(a){ var b=a*2; function bar(c){console.log(a,b,c);}bar(b*3);}foo(2);
