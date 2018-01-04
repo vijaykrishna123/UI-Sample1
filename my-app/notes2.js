05c6e50419e099750327d2afe61f53bd8515a093
vijaykrishna123 <nimmagadda.vijju@gmail.com>
2018-01-03 22:48:16 -0600

    notes2.js

--- my-app/notes2.js
+++ my-app/notes2.js
@@ -150,3 +150,84 @@ function Welcome(props) {
   );
 
   //output   Hi, apple, texas  
+//
+
+//composing components
+composing all into 1 HTMLHeadingElement
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  function App() { ///app 
+    return (
+      <div>
+          //mandatory within div tag
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+      </div>
+    );
+  }
+  
+  ReactDOM.render(
+    <App />, //define app function
+    document.getElementById('root')
+  );
+
+  //output
+  Hello, Sara
+  Hello, Cahal
+  Hello, Edite
+
+  ///
+  function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+  
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+          <img
+            className="Avatar"
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <div className="UserInfo-name">
+            {props.author.name}
+          </div>
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  }
+  
+  const comment = {
+    date: new Date(),
+    text:
+      'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl:
+        'http://placekitten.com/g/64/64',
+    },
+  };
+  ReactDOM.render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
+    document.getElementById('root')
+  );
+  //output
+  image
+  Hello Kitty
+  I hope you enjoy learning React!
+  1/3/2018
